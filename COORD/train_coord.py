c464c46de9c65f8843bc976c2193727cd2bab432
Guowener <3313589001@qq.com>
2022-11-08 16:02:36 +0800

    Add files via upload

--- COORD/train_coord.py
+++ COORD/train_coord.py
@@ -1,6 +1,8 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import torch
 import numpy as np
-from COORD.network import Predictor, AUTOCODER_array
+from COORD.network import Predictor, AUTOCODER_array, VAE
 from torch.utils.data import Dataset, DataLoader
 import os
 
@@ -16,11 +18,13 @@ class COORD_dataloader(Dataset):
         return len(self.all_inputs)
 
     def __getitem__(self, item):
+        random_array = np.random.normal(1, 0.02, size=(1, 3)).astype("float32")
+        random_array[0, 1] = random_array[0, 1] + (random_array[0, 1] - 1) / 2
         filename = self.all_inputs[item]
         vect = np.load(filename)
         vect_torch = torch.from_numpy(vect).cuda()
         if self.array_mode:
-            return torch.transpose(torch.reshape(vect_torch, shape=(vect_torch.shape[0]//3, 3)), dim0=0, dim1=1), filename
+            return torch.transpose(torch.reshape(vect_torch, shape=(vect_torch.shape[0]//3, 3)), dim0=0, dim1=1).cuda(), filename   # * torch.from_numpy(random_array.T)
         else:
             return vect_torch
 
@@ -50,64 +54,77 @@ class PreDataloader(Dataset):
         return torch.from_numpy(np.array([temperature, pressure, displacement], dtype="float32")).cuda(), vect_torch, filename
 
 
-def train_autocoder(type="BM", save=False, display=False, step=10):
-    trainset_dir = "I:\\DigitRubber_Dataset\\NPY\\COORD\\TRAIN\\"
-    validset_dir = "I:\\DigitRubber_Dataset\\NPY\\COORD\\VALID\\"
-    if save:
-        savedir = "I:\\DigitRubber_Dataset\\NPY\\VALID_RESULTS\\COORD\\CODER\\"
+def train_autocoder(type="BM", save_code=False, display=False, step=10):
+    trainset_dir = r"E:\Data\DATASET\SealDigitTwin\Results\0_input_npy\COORD_ALL\\"
+    validset_dir = r"E:\Data\DATASET\SealDigitTwin\COORD\VALID\\"
+    params_dir = './params/'
+    if save_code:
+        savedir_train = "E:\Data\DATASET\SealDigitTwin\CONTINUUM\TRAIN\\"
+        savedir_valid = "E:\Data\DATASET\SealDigitTwin\CONTINUUM\VALID\\"
     train_dl = DataLoader(COORD_dataloader(trainset_dir, type=type, array_mode=True),
                           batch_size=1,
                           shuffle=True)
     valid_dl = DataLoader(COORD_dataloader(validset_dir, type=type, array_mode=True),
                           batch_size=1,
                           shuffle=True)
-    lr = 1e-5
 
-    # Vector length of each layer
+    lr = 1e-5
     if type == "BM":
         vect_length = 15184
     elif type == "NB":
         vect_length = 13120
     else:
         vect_length = 26016
-    autocoder = AUTOCODER_array(vect_length=vect_length, code_length=24, num_layer=3).cuda()
+    autocoder = VAE(vect_length=vect_length, code_length=24, num_layer=2).cuda()
 
-    ## uncomment if there is pre-trained
-    # checkpoint = torch.load(type + "_coder_best.pth")
-    # autocoder.load_state_dict(checkpoint)
-    # loss_rec_all_valid = np.loadtxt(type + "_loss_valid.txt").tolist()
-    # loss_rec_all_train = np.loadtxt(type + "_loss_train.txt").tolist()
-    loss_rec_all_valid = [1]
-    loss_rec_all_train = []
+    if os.path.exists(params_dir + type + "_coder_last.pth"):
+        checkpoint = torch.load(params_dir + type + "_coder_best.pth")
+        autocoder.load_state_dict(checkpoint)
+        results_rec = np.loadtxt(params_dir + type + "_training_rec.txt")
+        loss_rec_all_valid = results_rec[:, 1].tolist()
+        loss_rec_all_train = results_rec[:, 2].tolist()
+        results_rec = results_rec.tolist()
+    else:
+        loss_rec_all_valid = [10000]
+        loss_rec_all_train = [10000]
+        results_rec = []
 
     optimizer = torch.optim.Adam(autocoder.parameters(), lr=lr)
-    epoch_n = 2000
-    loss_fn = torch.nn.MSELoss(reduce=True, reduction='mean')
+    epoch_n = 800
 
     loss_rec = []
     for epoch in range(epoch_n):
         epoch += 1
         autocoder.train()
 
-        for i, (vects, _) in enumerate(train_dl):
-            outputs = autocoder(vects.clone())
+        if epoch % 200 == 199:
+            lr = lr / 2
+            optimizer = torch.optim.Adam(autocoder.parameters(), lr=lr)
+
+        for i, (vects, fname) in enumerate(train_dl):
             autocoder.zero_grad()
-            loss = loss_fn(vects, outputs)
+            recon, mean, log_std = autocoder(vects.clone())
+            loss, recon_loss = autocoder.loss_function(recon, vects, mean, log_std)
             loss.backward()
             optimizer.step()
-            loss_rec.append(loss.item())
+            loss_rec.append(recon_loss.item())
+            if save_code:
+                code = mean[0].detach().cpu().numpy()
+                np.save(savedir_train + fname[0].split("\\")[-1], code)
+
 
         if epoch % step == 0 or epoch == epoch_n - 1:
             train_loss_mean = np.mean(np.array(loss_rec))
             loss_rec_all_train.append(train_loss_mean)
             loss_rec = []
             for i, (vects, file_name) in enumerate(valid_dl):
-                outputs = autocoder(vects.clone())
-                loss = loss_fn(vects, outputs)
-                loss_rec.append(loss.item())
-                '''For visualization'''
+                recon, mean, log_std = autocoder(vects.clone())
+                loss, recon_loss = autocoder.loss_function(recon, vects, mean, log_std)
+                loss_rec.append(recon_loss.item())
+
+                '''下面的代码是用于可视化的，训练的时候可以注释掉'''
                 if display:
-                    vect_outputs = outputs[0, :].detach().cpu().numpy()
+                    vect_outputs = recon[0, :].detach().cpu().numpy()
                     vect_init = vects[0, :].detach().cpu().numpy()
                     x_coord = [vect_outputs[3 * i] for i in range(vect_outputs.shape[0] // 3)]
                     y_coord = [vect_outputs[3 * i + 1] for i in range(vect_outputs.shape[0] // 3)]
@@ -126,25 +143,25 @@ def train_autocoder(type="BM", save=False, display=False, step=10):
                     plt.title("Shapes")
                     plt.legend()
                     plt.show()
-                if save and epoch > epoch_n-100:
-                    vect_outputs = outputs[0, :].detach().cpu().numpy()
-                    temp_name = savedir + "CODER_" + file_name[0].split("\\")[-1][:-4]+".csv"
-                    np.savetxt(temp_name, vect_outputs.T, delimiter=",")
+                if save_code:
+                    code = mean[0].detach().cpu().numpy()
+                    np.save(savedir_valid + file_name[0].split("\\")[-1], code)
+
 
             valid_loss_mean = np.mean(np.array(loss_rec))
+            results_rec.append([lr, train_loss_mean, valid_loss_mean])
 
             if valid_loss_mean < min(loss_rec_all_valid):
-                torch.save(autocoder.state_dict(), type + "_coder_best.pth")
-                np.savetxt(type + "_loss_valid.txt", np.array(loss_rec_all_valid))
-                np.savetxt(type + "_loss_train.txt", np.array(loss_rec_all_train))
+                torch.save(autocoder.state_dict(), params_dir + type + "_coder_best.pth")
+            np.savetxt(params_dir + type + "_training_rec.txt", np.array(results_rec))
             loss_rec_all_valid.append(valid_loss_mean)
             print("Epoch %d, Train Loss: %.5f, Valid Loss: %.5f"%(epoch, train_loss_mean, valid_loss_mean))
-            torch.save(autocoder.state_dict(), type + "_coder_last.pth")
+            torch.save(autocoder.state_dict(), params_dir + type + "_coder_last.pth")
 
 
 def train_predictor(type="BM", save=False, display=False, step=10):
-    trainset_dir = "I:\\DigitRubber_Dataset\\NPY\\COORD\\TRAIN\\"
-    validset_dir = "I:\\DigitRubber_Dataset\\NPY\\COORD\\VALID\\"
+    trainset_dir = r"E:\Data\DATASET\SealDigitTwin\Results\0_input_npy\COORD_ALL\\"
+    validset_dir = r"E:\Data\DATASET\SealDigitTwin\COORD\VALID\\"
     train_dl = DataLoader(PreDataloader(trainset_dir, type=type, array_mode=True),
                           batch_size=1,
                           shuffle=True)
@@ -160,48 +177,70 @@ def train_predictor(type="BM", save=False, display=False, step=10):
         vect_length = 26016
     if save:
         savedir = "I:\\DigitRubber_Dataset\\NPY\\VALID_RESULTS\\COORD\\PREDICTOR\\"
-    Code_predictor = Predictor(code_length=24, params_length=3, hiden_length=64).cuda()
-    autocoder = AUTOCODER_array(vect_length=vect_length, code_length=24, num_layer=3).cuda()
 
-    checkpoint = torch.load(type + "_coder_best.pth")
+    Code_predictor = Predictor(code_length=24, params_length=3, hiden_length=64).cuda()
+    autocoder = VAE(vect_length=vect_length, code_length=24, num_layer=2).cuda()
+
+    checkpoint = torch.load("./params/" + type + "_coder_best.pth")
     autocoder.load_state_dict(checkpoint)
     autocoder.eval()
 
-    # checkpoint = torch.load(type + "_Predictor_last.pth")
+    # checkpoint = torch.load(type + "_Predictor_best.pth")
     # Code_predictor.load_state_dict(checkpoint)
+    if os.path.exists("./params/" + type + "_Predictor_last.pth"):
+        checkpoint = torch.load("./params/" + type + "_Predictor_best.pth")
+        Code_predictor.load_state_dict(checkpoint)
+        results_rec = np.loadtxt("./params/" + type + "_predictor_training_rec.txt")
+        loss_rec_all_valid = results_rec[:, 1].tolist()
+        loss_rec_all_train = results_rec[:, 2].tolist()
+        results_rec = results_rec.tolist()
+    else:
+        loss_rec_all_valid = [10000]
+        loss_rec_all_train = [10000]
+        results_rec = []
+
     optimizer = torch.optim.Adam(Code_predictor.parameters(), lr=lr)
-    epoch_n = 2000
-    loss_fn = torch.nn.MSELoss(reduce=True, reduction='mean')
+    epoch_n = 500
+    loss_fn = torch.nn.MSELoss(reduce=True, reduction='sum')
 
-    loss_rec_all = [10]
-
-    loss_rec = []
     for epoch in range(epoch_n):
         epoch += 1
         Code_predictor.train()
 
+        if epoch % 100 == 99:
+            lr = lr / 2
+            optimizer = torch.optim.Adam(Code_predictor.parameters(), lr=lr)
+        # train_data = []
+        loss_rec = []
         for i, [inputs, vects, _] in enumerate(train_dl):
-            codes = autocoder(vects, coder=0)                   # using encoder block to predict code
+            codes, _ = autocoder.encode(vects)
             outputs = Code_predictor(inputs)
-            # predicted_shape = autocoder(outputs, coder=1)     # using decoder to predict if needed
+            # predicted_shape = autocoder(outputs, coder=1)
+            # temp_input_code_line = torch.cat([inputs, codes, outputs], dim=1)[0, :].detach().cpu().numpy()
+            # train_data.append(temp_input_code_line)
             Code_predictor.zero_grad()
-            loss = loss_fn(codes, outputs)                      # loss between predicted code and the encoded code
+            loss = loss_fn(codes, outputs)
             loss.backward()
             optimizer.step()
-
             loss_rec.append(loss.item())
+        # train_data = np.array(train_data)
+        # np.savetxt(type+"_train_codes_PRE_ADD.txt", train_data)
 
+        # valid_data = []
         if epoch % step == 0 or epoch == epoch_n - 1:
             train_loss_mean = np.mean(np.array(loss_rec))
             loss_rec = []
             for i, [inputs, vects, filename] in enumerate(valid_dl):
-                codes = autocoder(vects, coder=0)
+                codes, _ = autocoder.encode(vects)
                 outputs = Code_predictor(inputs)
-                shape_outputs = autocoder(outputs, coder=1)
+                # temp_input_code_line = torch.cat([inputs, codes, outputs], dim=1)[0, :].detach().cpu().numpy()
+                # valid_data.append(temp_input_code_line)
+                # print(inputs)
+                shape_outputs = autocoder.decode(outputs)
                 loss = loss_fn(codes, outputs)
                 loss_rec.append(loss.item())
 
-                '''for visualization'''
+                '''下面的代码是用于可视化的，训练的时候可以注释掉'''
                 if display:
                     vect_outputs = shape_outputs[0, :].detach().cpu().numpy()
                     vect_init = vects[0, :].detach().cpu().numpy()
@@ -223,65 +262,37 @@ def train_predictor(type="BM", save=False, display=False, step=10):
                     plt.title("Shapes of T: %d deg, P: %d kPa, D: %.2f mm"%(int(inputs_num[0]), int(inputs_num[1]), inputs_num[2]))
                     plt.legend()
                     plt.show()
-                if save and epoch > epoch_n-100:
-                    vect_outputs = shape_outputs[0, :].detach().cpu().numpy()
-                    temp_name = savedir + "PREDICTOR_" + filename[0].split("\\")[-1][:-4] + ".csv"
-                    np.savetxt(temp_name, vect_outputs.T, delimiter=",")
-
+                # if save and epoch > epoch_n-100:
+                #     vect_outputs = shape_outputs[0, :].detach().cpu().numpy()
+                #     temp_name = savedir + "PREDICTOR_" + filename[0].split("\\")[-1][:-4] + ".csv"
+                #     np.savetxt(temp_name, vect_outputs.T, delimiter=",")
             valid_loss_mean = np.mean(np.array(loss_rec))
-            if valid_loss_mean < min(loss_rec_all):
-                torch.save(Code_predictor.state_dict(), type + "_Predictor_best.pth")
-            loss_rec_all.append(valid_loss_mean)
-            print("Epoch %d, Train Loss: %.4f, Valid Loss: %.4f" % (epoch, train_loss_mean, valid_loss_mean))
-            torch.save(Code_predictor.state_dict(), type + "_Predictor_last.pth")
+            results_rec.append([lr, train_loss_mean, valid_loss_mean])
 
+            if valid_loss_mean < min(loss_rec_all_valid):
+                torch.save(Code_predictor.state_dict(), "./params/" + type + "_predictor_best.pth")
+                np.savetxt("./params/" + type + "_predictor_training_rec.txt", np.array(results_rec))
+            loss_rec_all_train.append(train_loss_mean)
+            loss_rec_all_valid.append(valid_loss_mean)
+            print("Epoch %d, Train Loss: %.5f, Valid Loss: %.5f" % (epoch, train_loss_mean, valid_loss_mean))
+            torch.save(Code_predictor.state_dict(), "./params/" + type + "_predictor_last.pth")
+            # valid_data = np.array(valid_data)
+            # np.savetxt(type+"_valid_codes_PRE.txt", valid_data)
 
-''' Run trained model in evaluation mode, from load condition to shape vector'''
-def eval_model(list_load, type, save=True):
-    if type == "BM":
-        vect_length = 15184
-    elif type == "NB":
-        vect_length = 13120
-    else:
-        vect_length = 26016
-    if save:
-        savedir = "I:\\DigitRubber_Dataset\\NPY\\VALID_RESULTS\\COORD\\PREDICTED\\"
-
-    Code_predictor = Predictor(code_length=24, params_length=3, hiden_length=64).cuda()
-    autocoder = AUTOCODER_array(vect_length=vect_length, code_length=24, num_layer=3).cuda()
-
-    checkpoint = torch.load(type + "_coder_best.pth")
-    autocoder.load_state_dict(checkpoint)
-    autocoder.eval()
-
-    checkpoint = torch.load(type + "_Predictor_best.pth")
-    Code_predictor.load_state_dict(checkpoint)
-    Code_predictor.eval()
-
-    for i in range(len(list_load)):
-        temp_input = torch.from_numpy(np.array(list_load[i], dtype="float32")).unsqueeze(0).cuda(0)
-        outputs = Code_predictor(temp_input)
-        shape_outputs = autocoder(outputs, coder=1)
-        vect_outputs = shape_outputs[0, :].detach().cpu().numpy().T
-        temp_name = savedir + "PREDICTED_" + str(i) + "_" + type+ "_"+str(list_load[i][0]) +"_"+ str(list_load[i][1])+"_"+ str(list_load[i][2])+"_COORD.csv"
-        np.savetxt(temp_name, vect_outputs, delimiter=",")
-
+            # valid_loss_mean = np.mean(np.array(loss_rec))
+            # if valid_loss_mean < min(loss_rec_all):
+            #     torch.save(Code_predictor.state_dict(), type + "_Predictor_best.pth")
+            # loss_rec_all.append(valid_loss_mean)
+            # print("Epoch %d, Train Loss: %.4f, Valid Loss: %.4f" % (epoch, train_loss_mean, valid_loss_mean))
+            # torch.save(Code_predictor.state_dict(), type + "_Predictor_last.pth")
 
 
 if __name__ == '__main__':
-    # train_autocoder(type="BM", save=True, display=False, step=10)
-    # train_autocoder(type="PR", save=True, display=False, step=10)
-    # train_autocoder(type="NB", save=True, display=False, step=10)
-    # train_predictor(type="BM", save=True, display=False, step=10)
-    # train_predictor(type="PR", save=True, display=False, step=10)
-    # train_predictor(type="NB", save=True, display=False, step=10)
-
-    arr = np.loadtxt(r"I:\DigitRubber_Dataset\NPY\VALID_RESULTS\\seriel2.txt", delimiter="\t", dtype="float32")
-    arr = arr[:, [1, 2, 0]]
-    eval_model(list_load=arr.tolist(),
-               type="BM")
-    eval_model(list_load=arr.tolist(),
-               type="NB")
-    eval_model(list_load=arr.tolist(),
-               type="PR")
+    # train_autocoder(type="BM", display=False, step=10)
+    # train_autocoder(type="PR", display=True, step=10)
+    # train_autocoder(type="NB", display=True, step=10)
+    #
+    train_predictor(type="BM", save=False, display=False, step=10)
+    train_predictor(type="PR", save=False, display=False, step=10)
+    train_predictor(type="NB", save=False, display=False, step=10)
 
